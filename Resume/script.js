6d6199a9ce38716eabe48ecb17c445a43ec802c4
AyushChauhan3757 <ayushc262@gmail.com>
2023-09-02 23:53:06 +0530

    Finally Complete

--- Resume/script.js
+++ Resume/script.js
@@ -1,28 +1,82 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
+// Typing Animation
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-    showPopup(true);
-  }
+var typed = new Typed(".typing", {
+    strings : ["Web Designer", "UI/UX Designer", "Front-End Developer", "Gamer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+
+// Aside
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll("li"),
+totalNavList = navList.length;
+allSection = document.querySelectorAll(".section"),
+totalSection = allSection.length;
+for(let i = 0; i < totalNavList; i++){
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function(){
+        removeBackSection();
+        for(let j = 0; j < totalNavList; j++) {
+            if(navList[j].querySelector("a").classList.contains("active")) {
+                addBackSection(j);
+                //allSection[j].classList.add("back-section");
+            }
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active");
+        showSection(this);
+        if(window.innerWidth < 1200) {
+            asideSectionTogglerBtn();
+        }
+    })
+}
+function showSection(element) {
+    for(let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active");
 }
 
-function showPopup(bool) {
-  if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
-  } else {
-    document.getElementById('popup').style.visibility = 'hidden'
-  }
+function removeBackSection() {
+    for(let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove("back-section");
+    }
+}
+
+function addBackSection(index) {
+    allSection[index].classList.add("back-section");
+}
+
+function updateNav(element) {
+    for(let i = 0; i < totalNavList; i++) {
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]) {
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+    element.classList.add("active");
+}
+
+document.querySelector(".hire-me").addEventListener("click", function() {
+    const SectionIndex = this.getAttribute("data-section-index");
+    showSection(this);
+    updateNav(this);
+    removeBackSection();
+    addBackSection(SectionIndex);
+});
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+    asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+    aside.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for(let i = 0; i < totalSection; i++) {
+        allSection[i].classList.toggle("open");
+    }
 }
---
+++ Resume/style-switcher.js
@@ -0,0 +1,44 @@
+// Toggle Style Switcher
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggle.addEventListener('click', () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+//Hide Style Switcher on Scroll
+
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+// Theme Colors
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+// Theme Light and Dark
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("light");
+});
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("light")) {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }else {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+});
