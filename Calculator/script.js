e561bd5f9d26a336206b5e608a2b312668c23f5b
AyushChauhan3757 <ayushc262@gmail.com>
2023-09-08 11:36:39 +0530

    added decimal

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,8 @@
-let runningTotal = 0;
-let buffer = "0";
-let previousOperator;
+// Initialize variables to manage calculator state
+let runningTotal = 0; // Stores the running total of calculations
+let buffer = "0"; // Stores the current input or number
+let previousOperator; // Stores the previous operator pressed
+let decimalFlag = false; // Flag to track if a decimal point has been entered
 
 // Get the calculator screen element
 const screen = document.querySelector('.screen');
@@ -25,17 +27,19 @@ function handleSymbol(symbol) {
             // Clear the buffer and runningTotal
             buffer = '0';
             runningTotal = 0;
+            decimalFlag = false; // Reset the decimal flag
             break;
         case '=':
             // If there's a previous operator, perform the calculation
             if (previousOperator === null) {
                 return;
             }
-            flushOperation(parseInt(buffer));
+            flushOperation(parseFloat(buffer)); // Parse buffer as a float for decimal support
             // Reset previousOperator, update buffer with the result, and reset runningTotal
             previousOperator = null;
-            buffer = runningTotal;
+            buffer = runningTotal.toString();
             runningTotal = 0;
+            decimalFlag = buffer.includes('.'); // Check if the result contains a decimal
             break;
         case '←':
             // Handle backspace by removing the last character from the buffer
@@ -43,6 +47,7 @@ function handleSymbol(symbol) {
                 buffer = '0';
             } else {
                 buffer = buffer.substring(0, buffer.length - 1);
+                decimalFlag = buffer.includes('.'); // Check if the remaining buffer contains a decimal
             }
             break;
         case '+':
@@ -52,6 +57,13 @@ function handleSymbol(symbol) {
             // Handle math operators (+, −, ×, ÷)
             handleMath(symbol);
             break;
+        case '.':
+            // Handle decimal point input
+            if (!decimalFlag) {
+                buffer += '.';
+                decimalFlag = true;
+            }
+            break;
     }
 }
 
@@ -60,37 +72,53 @@ function handleMath(symbol) {
     if (buffer === '0') {
         return;
     }
-    const intBuffer = parseInt(buffer);
+    const floatBuffer = parseFloat(buffer); // Parse buffer as a float for decimal support
     if (runningTotal === 0) {
-        runningTotal = intBuffer;
+        runningTotal = floatBuffer;
     } else {
-        flushOperation(intBuffer);
+        flushOperation(floatBuffer);
     }
     // Store the previous operator for later use
     previousOperator = symbol;
     buffer = '0';
+    decimalFlag = false; // Reset the decimal flag
 }
 
 // Function to perform the calculation based on the previous operator
-function flushOperation(intBuffer) {
+function flushOperation(floatBuffer) {
     if (previousOperator === '+') {
-        runningTotal += intBuffer;
+        runningTotal += floatBuffer;
     } else if (previousOperator === '−') {
-        runningTotal -= intBuffer;
+        runningTotal -= floatBuffer;
     } else if (previousOperator === '×') {
-        runningTotal *= intBuffer;
+        runningTotal *= floatBuffer;
     } else if (previousOperator === '÷') {
-        runningTotal /= intBuffer;
+        if (floatBuffer === 0) {
+            // Handle division by zero
+            alert("Error: Division by zero");
+            clearCalculator();
+            return;
+        }
+        runningTotal /= floatBuffer;
     }
 }
 
-// Function to handle numbers
+// Function to handle numbers and decimal point input
 function handleNumber(numberString) {
-    if (buffer === "0") {
+    if (buffer === "0" && numberString !== "0") {
         buffer = numberString;
     } else {
         buffer += numberString;
     }
+}
+
+// Function to clear the calculator
+function clearCalculator() {
+    buffer = '0';
+    runningTotal = 0;
+    previousOperator = null;
+    decimalFlag = false;
+    screen.innerText = buffer;
 }
 
 // Initialize the calculator by adding click event listeners to buttons
