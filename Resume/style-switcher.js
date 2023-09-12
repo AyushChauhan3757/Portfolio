d03b278b5407863654cfdbb42259371217428cdb
AyushChauhan3757 <ayushc262@gmail.com>
2023-09-12 15:14:07 +0530

    changed default theme

--- Resume/style-switcher.js
+++ Resume/style-switcher.js
@@ -33,12 +33,12 @@ const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
 });
 window.addEventListener("load", () => {
-    if(document.body.classList.contains("light")) {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }else {
+    if(document.body.classList.contains("dark")) {
         dayNight.querySelector("i").classList.add("fa-sun");
+    }else {
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
 });
